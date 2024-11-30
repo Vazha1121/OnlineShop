efb156ad6883ebefd290a4cff6ff64d841448424
Vazha Iakobashvili <vajikoiakobashvili9@gmail.com>
2024-11-30 16:32:06 +0400

    prods

--- src/app/Routes/laptops/laptops.component.ts
+++ src/app/Routes/laptops/laptops.component.ts
@@ -6,48 +6,60 @@ import { ApiService } from '../../Services/api.service';
 
 @Component({
   selector: 'app-laptops',
-  imports: [RouterModule,FormsModule,CommonModule],
+  imports: [RouterModule, FormsModule, CommonModule],
   templateUrl: './laptops.component.html',
-  styleUrl: './laptops.component.scss'
+  styleUrl: './laptops.component.scss',
 })
-export class LaptopsComponent implements OnInit{
-  constructor(public api: ApiService){}
+export class LaptopsComponent implements OnInit {
+  constructor(public api: ApiService) {}
   ngOnInit(): void {
-    this.brandsApi()
+    this.brandsApi();
+    this.getProds(this.pageID);
   }
-public minPrice: number = 0;
-public maxPrice: number = 6000;
-public priceGap: number = 100;
+  public minPrice: number = 0;
+  public maxPrice: number = 6000;
+  public priceGap: number = 100;
 
-get progressLeft(): string {
-  return `${(this.minPrice / 6000) * 100}%`
-}
-get progressRight():string {
-  return `${100 - (this.maxPrice/ 6000) *100}%`
-}
+  get progressLeft(): string {
+    return `${(this.minPrice / 6000) * 100}%`;
+  }
+  get progressRight(): string {
+    return `${100 - (this.maxPrice / 6000) * 100}%`;
+  }
 
-  onInputChange(){
-    if(this.maxPrice - this.minPrice < this.priceGap){
+  onInputChange() {
+    if (this.maxPrice - this.minPrice < this.priceGap) {
       this.minPrice = Math.min(this.minPrice, this.maxPrice - this.priceGap);
       this.maxPrice = Math.max(this.maxPrice, this.minPrice + this.priceGap);
     }
   }
-  onRangeChange(){
-    if(this.maxPrice - this.minPrice < this.priceGap){
-      this.maxPrice = this.minPrice + this.priceGap
+  onRangeChange() {
+    if (this.maxPrice - this.minPrice < this.priceGap) {
+      this.maxPrice = this.minPrice + this.priceGap;
     }
   }
-  public brandUl:any;
-  brandsApi(){
+
+  public brandUl: any;
+  brandsApi() {
     this.api.brands().subscribe({
-      next: (data:any)=> {
-        console.log(data);
+      next: (data: any) => {
         this.brandUl = data;
-        
       },
-      error: (err:any) => 
-        console.log(err)
-        
-    })
+      error: (err: any) => console.log(err),
+    });
+  }
+  public id: any = 1;
+  public pageSize: any = 10;
+  public prods: any;
+  public pageRaod: any = [1, 2, 3];
+  public pageID: any = 0;
+  getProds(ID: any) {
+    ID++;
+    this.pageID = ID;
+    this.api.getCategoryWithId(this.id, this.pageID, this.pageSize).subscribe({
+      next: (data: any) => {
+        this.prods = data.products;
+      },
+    });
   }
 }
